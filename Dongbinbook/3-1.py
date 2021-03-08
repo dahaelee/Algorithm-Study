0ae49c767189cf602292d64c2188f3fac29334a8
dahaelee <dah3132@gmail.com>
2021-03-08 21:15:35 +0900

    chapter 3

--- Dongbinbook/3-1.py
+++ Dongbinbook/3-1.py
@@ -3,8 +3,8 @@ cnt = 0
 
 coins = [500, 100, 50, 10]
 
-for i in coins:
-    cnt += n // i
-    n %= i
+for coin in coins:
+    cnt += n // coin
+    n %= coin
 
 print(cnt)
--- Dongbinbook/3-3.py
+++ Dongbinbook/3-3.py
@@ -10,5 +10,5 @@ ans = max(mins)
 
 print(ans)
 
-# 각 행의 min값 리스트를 만들어 거기서 max값을 구했는데,
+# 나는 각 행의 min값 리스트를 만들어 거기서 max값을 구했는데,
 # 전역변수 ans=0으로 설정하고, for문 내에서 max(ans,행의min값) 으로 max값을 갱신시켜나가는 방법도...
