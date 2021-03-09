1268bf366388506ba7e103523cca3440579cd528
dahaelee <dah3132@gmail.com>
2021-03-09 10:56:53 +0900

    chapter 3

--- Dongbinbook/3-4.py
+++ Dongbinbook/3-4.py
@@ -12,4 +12,23 @@ while n > 1:
         n = n - 1
         cnt += 1
 
+'''
+# 수학적으로 풀기
+
+while True:
+    # n이 k로 나누어질때까지 1 빼는데, 그걸 한번에
+    target = (n//k) * k
+    cnt += (n-target)
+    n = target
+    
+    if n < k:
+        break;
+    
+    n //= k
+    cnt += 1
+
+# 남은 수가 1 될때까지 1씩 빼는 횟수
+cnt += (n-1)
+'''
+
 print(cnt)
