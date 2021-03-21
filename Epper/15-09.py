acd940a4e422d49603881e15b3681d5933e1de39
dahaelee <dah3132@gmail.com>
2021-03-21 13:03:08 +0900

    update

--- Epper/15-09.py
+++ Epper/15-09.py
@@ -2,12 +2,11 @@ from collections import deque
 
 def solution(n, r, goal, N, R):
     queue = deque()
+    graph = [[] for _ in range(N+1)]
     indegree = [0] * (N + 1)
-    graph = [[] for _ in range(N + 1)]
 
-    # 인덱스를 노드 번호에 맞추기
-    for i in range(len(r)):
-        graph[i + 1] = r[i]
+    for x in r:
+        graph[x[0]].append(x[1])
 
     # 진입차수
     for x in graph:
@@ -20,7 +19,7 @@ def solution(n, r, goal, N, R):
     # 진입차수가 0인 공정을 큐에 삽입하고 최대 시간을 해당 공정의 시간으로
     for i in range(1, N + 1):
         if indegree[i] == 0:
-            time[i] = n[i - 1] # 리스트 n만 인덱스 안맞아서 맞춰줘야 함
+            time[i] = n[i - 1]
             queue.append(i)
 
     while queue:  # 큐가 빌 때까지
@@ -28,7 +27,7 @@ def solution(n, r, goal, N, R):
 
         # 연결되어 있는 노드들의 진입차수에서 1을 빼고, 0이 되면 큐에 삽입
         for i in graph[now]:
-            # 최대 시간은 max(지금까지 계산된 시간, 이전 공정까지 걸리는 시간 + 지금 공정 시간)
+            # 지금까지 계산된 시간 or 이전 공정까지 걸리는 시간 + 지금 공정 시간
             time[i] = max(time[i], time[now] + n[i - 1])
             indegree[i] -= 1
             if indegree[i] == 0:
@@ -38,10 +37,9 @@ def solution(n, r, goal, N, R):
 
 N, R = map(int, input().split())
 n = list(map(int, input().split()))
-r = [[] for _ in range(N - 1)]
+r = []
 for _ in range(R):
-    a, b = map(int, input().split())
-    r[a - 1].append(b)
+    r.append(list(map(int, input().split())))
 goal = int(input())
 
 result = solution(n, r, goal, N, R)
