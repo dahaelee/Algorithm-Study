add0492576168dcfe3fbcced791366697c9db797
dahaelee <dah3132@gmail.com>
2021-03-26 00:10:48 +0900

    update

--- Programmers/고득점 Kit/해시_완주하지 못한 선수.py
+++ Programmers/고득점 Kit/해시_완주하지 못한 선수.py
@@ -1,16 +1,6 @@
 def solution(participant, completion):
-    dict = {}
     for p in participant:
-        dict[p] = 0
-
-    # 참가자를 하나씩 확인하면서
-    # 완주자 목록에 있으면 완주자 목록에서 제거(동명이인때문), 없으면 value값 1로 변경
-    for p in dict:
         if p in completion:
             completion.remove(p)
         else:
-            dict[p] = 1
-
-    print(dict)
-
-    return
+            return p
