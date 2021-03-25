0ac01565a976dc91e284417bd66dfa080185944e
dahaelee <dah3132@gmail.com>
2021-03-26 00:43:04 +0900

    update

--- Programmers/고득점 Kit/해시_완주하지 못한 선수.py
+++ Programmers/고득점 Kit/해시_완주하지 못한 선수.py
@@ -1,6 +1,27 @@
 def solution(participant, completion):
+    dic = {}
+
+    for p in participant:
+        if p in dic:
+            dic[p] += 1
+        else:
+            dic[p] = 1
+
+    for c in completion:
+        dic[c] -= 1
+
+    for p in dic:
+        if dic[p] > 0:
+            return p
+
+'''
+# 정확성은 100프로지만 효율성 검사를 통과하지 못함.
+# remove(n)의 시간복잡도가 O(n)이라 for문 안에 들어가면 O(n^2)이 되기 때문
+
+def solution(participant, completion):
     for p in participant:
         if p in completion:
             completion.remove(p)
         else:
             return p
+'''
