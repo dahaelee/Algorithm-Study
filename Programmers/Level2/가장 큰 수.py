6491c26cb8c2bde9a04f221994dfb43fe9cb873f
dahaelee <dah3132@gmail.com>
2021-05-14 00:25:30 +0900

    update

--- Programmers/Level2/가장 큰 수.py
+++ Programmers/Level2/가장 큰 수.py
@@ -6,5 +6,5 @@ def solution(numbers):
     # 원소는 1000 이하이므로 최소 3번 반복해줘야 함.
     numbers.sort(key=lambda x: x * 3, reverse=True)
 
-    # 000 같은 경우 0으로 바꾸기 위해 int로 변환했다가 다시 str로
+    # 000 같은 경우 0으로 바꾸기 위해 int로 변환했다가 다시 str로 변환
     return str(int(''.join(numbers)))
