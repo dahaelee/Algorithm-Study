017a2ab185933ce32d770fbd6b81d284b21d6d62
dahaelee <dah3132@gmail.com>
2021-05-17 00:12:19 +0900

    update

--- Programmers/Level1/실패율.py
+++ Programmers/Level1/실패율.py
@@ -1,5 +1,26 @@
+def solution(N, stages):
+    fail = {}
+    users = len(stages)
+
+    # 스테이지마다 실패율 계산
+    # 실패율 = 실패한 유저 수 / 도달한 유저 수
+    # 1스테이지에서는 도달한 유저 수 = 전체 유저 수
+    # 다음 단계에 도달한 유저 수는 실패 유저수를 빼가면서 구함
+
+    for stage in range(1, N + 1):
+        if users:
+            failed = stages.count(stage)
+            fail[stage] = failed / users
+            users -= failed
+
+        else:  # 스테이지에 도달한 사람 없으면 실패율 0
+            fail[stage] = 0
+
+    return sorted(fail, key=lambda x: fail[x], reverse=True)
+
+
 '''
-정확도는 통과하지만 테케 하나에서 시간초과가 나는 풀이
+처음 푼 정확도는 통과하지만 테케 하나에서 시간초과가 나는 풀이
 이중 for문은 최대한 피하고, 불가피할 때는 시간복잡도를 체크하자
 
 def solution(N, stages):
@@ -26,24 +47,3 @@ def solution(N, stages):
 
     return [x[0] for x in a]
 '''
-
-
-def solution(N, stages):
-    fail = {}
-    users = len(stages)
-
-    # 스테이지마다 실패율 계산
-    # 실패율 = 실패한 유저 수 / 도달한 유저 수
-    # 1스테이지에서는 도달한 유저 수 = 전체 유저 수
-    # 다음 단계에 도달한 유저 수는 실패 유저수를 빼가면서 구함
-
-    for stage in range(1, N + 1):
-        if users:
-            failed = stages.count(stage)
-            fail[stage] = failed / users
-            users -= failed
-
-        else:  # 스테이지에 도달한 사람 없으면 실패율 0
-            fail[stage] = 0
-
-    return sorted(fail, key=lambda x: fail[x], reverse=True)
