e022c75200a1aa67723ba42f4a712afcd30adf44
dahaelee <dah3132@gmail.com>
2021-05-16 22:15:52 +0900

    update

--- Programmers/Level1/크레인 인형뽑기 게임.py
+++ Programmers/Level1/크레인 인형뽑기 게임.py
@@ -1,20 +1,27 @@
 def solution(board, moves):
-    answer = 0
+    nboard = [[] for _ in range(len(board))]
     stack = []
+    answer = 0
 
-    def check(item):
-        if len(stack) > 0 and stack[-1] == item:
-            stack.pop()
-            return 2
-        else:
-            stack.append(item)
-            return 0
+    for r in board:
+        for i, x in enumerate(r):
+            if x == 0:
+                continue
+            else:
+                nboard[i].append(x)
 
     for i in moves:
-        for index in range(len(board)):
-            if board[index][i - 1] != 0:
-                answer += check(board[index][i - 1])
-                board[index][i - 1] = 0
-                break
+        if nboard[i - 1]:
+            x = nboard[i - 1].pop(0)  # 해당 열에서 제일 위에 있는 인형
+
+            # 스택이 비어있으면 바로 넣고, 아니면 비교
+            if not stack:
+                stack.append(x)
+            else:
+                if x == stack[-1]:
+                    stack.pop()
+                    answer += 2
+                else:
+                    stack.append(x)
 
     return answer
