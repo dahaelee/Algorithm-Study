64ad3ac14c57e6cf1753026a5e2b9a611ee83495
dahaelee <dah3132@gmail.com>
2021-05-16 21:00:32 +0900

    update

--- Programmers/Level1/키패드 누르기.py
+++ Programmers/Level1/키패드 누르기.py
@@ -1,33 +1,33 @@
 def solution(numbers, hand):
     answer = ''
-    lastL = 10
-    lastR = 12
 
-    for n in numbers:
-        if n in [1, 4, 7]:
+    left_now = 10
+    right_now = 12
+
+    for x in numbers:
+        x = 11 if x == 0 else x  # 0이면 11로 바꿔주기
+
+        if x in [1, 4, 7]:
             answer += 'L'
-            lastL = n
-        elif n in [3, 6, 9]:
+            left_now = x
+        elif x in [3, 6, 9]:
             answer += 'R'
-            lastR = n
+            right_now = x
         else:
-            n = 11 if n == 0 else n
+            ldis = sum(divmod(abs(x - left_now), 3))
+            rdis = sum(divmod(abs(x - right_now), 3))
 
-            absL = abs(n - lastL)
-            absR = abs(n - lastR)
-
-            if sum(divmod(absL, 3)) > sum(divmod(absR, 3)):
-                answer += 'R'
-                lastR = n
-            elif sum(divmod(absL, 3)) < sum(divmod(absR, 3)):
+            if ldis < rdis:
                 answer += 'L'
-                lastL = n
+                left_now = x
+            elif ldis > rdis:
+                answer += 'R'
+                right_now = x
             else:
                 if hand == 'left':
                     answer += 'L'
-                    lastL = n
+                    left_now = x
                 else:
                     answer += 'R'
-                    lastR = n
-
+                    right_now = x
     return answer
