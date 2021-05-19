c2a50fb8967be2a9e800c4314ae4c87c413dc334
dahaelee <dah3132@gmail.com>
2021-05-19 22:25:16 +0900

    update

--- Programmers/Level1/신규 아이디 추천.py
+++ Programmers/Level1/신규 아이디 추천.py
@@ -4,9 +4,9 @@ def solution(new_id):
     st = new_id.lower() # 1단계
     st = re.sub('[^a-z0-9-_.]', '', st) # 2단계
     st = re.sub('\.+', '.', st) # 3단계
-    st = re.sub('^[.]|[.]$', '', st) # 4단계
+    st = re.sub('^\.|\.$', '', st) # 4단계
     st = 'a' if len(st) == 0 else st[:15] # 5단계 + 6단계(1)
-    st = re.sub('[.]$', '', st) # 6단계(2)
+    st = re.sub('\.$', '', st) # 6단계(2)
     st += (st[-1] * (3 - len(st))) if len(st) < 3 else '' # 7단계
 
     return st
