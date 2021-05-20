446144bb1cbed52178cb1ca205bbb359405d19bf
dahaelee <dah3132@gmail.com>
2021-05-20 22:32:53 +0900

    update

--- Programmers/Level2/N개의 최소공배수.py
+++ Programmers/Level2/N개의 최소공배수.py
@@ -4,35 +4,14 @@ def gcd(a, b):
     return a
 
 
+def lcm(a, b):
+    return a * b / gcd(a, b)
+
+
 def solution(arr):
     answer = arr[0]
 
     for x in arr:
-        answer = answer * x / gcd(answer, x)
+        answer = lcm(x, answer)
 
     return answer
-
-
-'''
-def gcd(a, b):
-    while b > 0:
-        a, b = b, a % b 
-    return a
-
-def lcm(a,b):
-    return a * b / gcd(a, b)
-
-def solution(arr):
-
-    if len(arr) == 1:
-        return(arr[0])
-
-    else: 
-        d = [0] * len(arr)
-        d[1] = lcm(arr[0], arr[1])
-
-        for i in range(2, len(arr)):
-            d[i] = lcm(d[i-1], arr[i])
-
-        return d[-1]
-'''
