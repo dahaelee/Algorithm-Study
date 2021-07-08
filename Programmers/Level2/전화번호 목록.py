d395ca390b4aff88569fa21060940d5b74114fdb
dahaelee <dah3132@gmail.com>
2021-07-09 01:02:03 +0900

    update

--- Programmers/Level2/전화번호 목록.py
+++ Programmers/Level2/전화번호 목록.py
@@ -1,9 +1,10 @@
 def solution(phone_book):
     phone_book.sort()
 
-    temp = phone_book[0]
-    for i in range(1, len(phone_book)):
-        if phone_book[i][:len(temp)] == temp:
+    # 정렬된 리스트에서 붙어 있는 것들끼리 검사
+    for i in range(len(phone_book) - 1):
+        plen = len(phone_book[i])
+        if phone_book[i] == phone_book[i + 1][:plen]:
             return False
 
     return True
