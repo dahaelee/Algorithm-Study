f641cc59dc8fcefb26fb9bfc0a811c15c55b74ab
dahaelee <dah3132@gmail.com>
2021-08-02 15:41:08 +0900

    update

--- Leetcode/827. Making A Large Island.py
+++ Leetcode/827. Making A Large Island.py
@@ -5,7 +5,9 @@ class Solution:
     def largestIsland(self, grid: List[List[int]]) -> int:
         n = len(grid)
 
-        def check(x, y):
+        def get_valid_next(x, y):
+            valid = []
+
             dx = [1, -1, 0, 0]
             dy = [0, 0, 1, -1]
 
@@ -13,10 +15,10 @@ class Solution:
                 nx = x + dx[i]
                 ny = y + dy[i]
 
-                if nx < 0 or nx >= n or ny < 0 or ny >= n:
-                    continue
+                if nx >= 0 and nx < n and ny >= 0 and ny < n:
+                    valid.append((nx, ny))
 
-                yield nx, ny
+            return valid
 
         def bfs(x, y, island):
             q = deque()
@@ -28,7 +30,7 @@ class Solution:
             while q:
                 x, y = q.popleft()
 
-                for nx, ny in check(x, y):
+                for nx, ny in get_valid_next(x, y):
                     if grid[nx][ny] == 1:
                         grid[nx][ny] = island
                         area += 1
@@ -36,10 +38,19 @@ class Solution:
 
             return area
 
+        # 전체 0이거나 전체 1인 경우 먼저 거르기
+        total_sum = 0
+        for row in grid:
+            total_sum += sum(row)
+
+        if total_sum == 0:
+            return 1
+        elif total_sum == n ** 2:
+            return n ** 2
+
         # 모든 연결된 섬 플러드필, 섬에 들어가는 지점을 섬 번호로 변경
         island = 2
         areas = {}
-        has_zero = False
 
         for i in range(n):
             for j in range(n):
@@ -47,21 +58,20 @@ class Solution:
                     areas[island] = bfs(i, j, island)
                     island += 1
 
-        # 0이면 인접한 섬끼리 더해주면서 최대값 갱신, 이때 인접한 섬이 한 섬일 경우 고려
+        # 0이면 인접한 섬끼리 더해주면서 최대값 갱신
         answer = 1
         for x in range(n):
             for y in range(n):
                 if grid[x][y] == 0:
-                    has_zero = True
-                    near_island = []
+                    near_islands = []
 
-                    for nx, ny in check(x, y):
+                    for nx, ny in get_valid_next(x, y):
                         if grid[nx][ny] > 1:
-                            near_island.append(grid[nx][ny])
+                            near_islands.append(grid[nx][ny])
 
-                    if near_island:
-                        near_island = list(set(near_island))
-                        near_area = [areas[x] for x in near_island]
-                        answer = max(answer, sum(near_area) + 1)
+                    if near_islands:
+                        near_islands = list(set(near_islands))
+                        near_areas = [areas[island] for island in near_islands]
+                        answer = max(answer, sum(near_areas) + 1)
 
-        return answer if has_zero else n ** 2
+        return answer
