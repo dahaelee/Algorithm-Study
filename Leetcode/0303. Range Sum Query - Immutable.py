ca1aadd85b7e2bb3918ce8df7a15267765f654e4
dahaelee <dah3132@gmail.com>
2021-08-17 09:25:59 +0900

    update

--- Leetcode/0303. Range Sum Query - Immutable.py
+++ Leetcode/0303. Range Sum Query - Immutable.py
@@ -1,9 +1,11 @@
 class NumArray:
     def __init__(self, nums: List[int]):
-        for i in range(1, len(nums)):
+        nums = [0] + nums
+
+        for i in range(len(nums)):
             nums[i] += nums[i - 1]
 
-        self.nums = [0] + nums
+        self.nums = nums
 
     def sumRange(self, left: int, right: int) -> int:
         return self.nums[right + 1] - self.nums[left]
